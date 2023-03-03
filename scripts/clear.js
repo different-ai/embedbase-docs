03504f868ec558bdc3424a388ae0bfca85162bf4
Louis Beaumont <louis.beaumont@gmail.com>
2023-03-03 11:16:22 +0000

    chore: sync

--- scripts/clear.js
+++ scripts/clear.js
@@ -1,7 +1,12 @@
 const fetch = require("node-fetch");
 
 const url = "https://embedbase-hosted-usx5gpslaq-uc.a.run.app";
-const vaultId = "embedbase-documentation";
+const vaultId = "documentation";
+try {
+    require("dotenv").config();
+} catch (e) {
+    console.log("No .env file found" + e);
+}
 const apiKey = process.env.EMBEDBASE_API_KEY;
 const clear = async () => {
     const response = await fetch(url + "/v1/" + vaultId + "/clear", {
